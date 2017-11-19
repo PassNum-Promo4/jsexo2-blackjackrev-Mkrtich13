8f43fa007f80d1e42cf0660e0a048b12b711c2aa
Mkrtich13 <hovanisianm@gmail.com>
2017-11-19 16:42:51 +0100

    correction et ajout valeur aléatoire dans le HTML.

--- js/blackjack.js
+++ js/blackjack.js
@@ -1,13 +1,15 @@
-// compare user number with computer number.
+// a function for compare user number with computer number.
 function compareBlackJ() {
   var random = Math.floor(Math.random()*11);
   var number = document.getElementById("formBlackJ__number").value;
+  // show random value in html.
+  var randomResult = document.getElementById("al").innerHTML = random;
 
-
-  if(random <= number ) {
-    document.getElementById("result").innerHTML = "Vous avez perdu";
-  } else if (random > number ){
+  // game condition.
+  if(random < number ) {
     document.getElementById("result").innerHTML = "Vous avez gagné";
+  } else if (random >= number ) {
+    document.getElementById("result").innerHTML = "Vous avez perdu";
   }
 
 
